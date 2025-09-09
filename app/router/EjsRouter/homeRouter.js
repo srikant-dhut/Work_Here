7fc73b16b936bf03336ccd2d035f9f57965ad979
srikant-dhut <119411185+srikant-dhut@users.noreply.github.com>
2025-09-09 19:01:07 +0530

    Update homeRouter.js
    con now

--- app/router/EjsRouter/homeRouter.js
+++ app/router/EjsRouter/homeRouter.js
@@ -4,5 +4,8 @@ const homeController = require("../../controller/EjsController/HomeController");
 
 
 router.get("/", homeController.homePage);
+router.get("/jobs", homeController.allJobsPage);
+router.get("/freelancers", homeController.allFreelancersPage);
+router.get("/blog", homeController.blogPage);
 
 module.exports = router;
